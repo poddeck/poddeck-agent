dbdb0d1b1672f05b130ebf1197503072b7561676
Lukas <contact@lukasbreuer.de>
2025-11-13 14:16:58 +0100

    Implement AgentApplication

---
+++ src/main/java/io/poddeck/agent/AgentApplication.java
@@ -0,0 +1,30 @@
+package io.poddeck.agent;
+
+import com.google.inject.Guice;
+import io.poddeck.agent.communication.CommunicationClient;
+import io.poddeck.common.event.EventExecutor;
+import io.poddeck.common.log.Log;
+
+public class AgentApplication {
+  /**
+   * The starting point where the application is executed
+   * @param args The arguments that are passed into the application
+   */
+  public static void main(String[] args) {
+    var injector = Guice.createInjector(AgentModule.create());
+    var log = injector.getInstance(Log.class);
+    Thread.setDefaultUncaughtExceptionHandler((thread, throwable) ->
+      log.processError(throwable));
+    try {
+      log.info("Initializing PodDeck - Agent");
+      var eventExecutor = injector.getInstance(EventExecutor.class);
+      log.info("Connecting to core...");
+      var client = injector.getInstance(CommunicationClient.class);
+      client.connect();
+      log.info("Successfully connected to core");
+      log.info("Successfully booted PodDeck - Agent");
+    } catch (Exception exception) {
+      log.processError(exception);
+    }
+  }
+}
