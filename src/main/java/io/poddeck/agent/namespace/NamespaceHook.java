372a95f418b24d9f74bfc3fb16fb2451505f3475
Lukas <contact@lukasbreuer.de>
2025-12-01 11:27:51 +0100

    Implement NamespaceHook

---
+++ src/main/java/io/poddeck/agent/namespace/NamespaceHook.java
@@ -0,0 +1,30 @@
+package io.poddeck.agent.namespace;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import io.poddeck.agent.application.ApplicationLaunchEvent;
+import io.poddeck.agent.communication.service.ServiceRepository;
+import io.poddeck.common.*;
+import io.poddeck.common.event.EventHook;
+import io.poddeck.common.event.Hook;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+
+@Singleton
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE, onConstructor = @__({@Inject}))
+public final class NamespaceHook implements Hook {
+  private final ServiceRepository serviceRepository;
+  private final NamespaceListService namespaceListService;
+  private final NamespaceCreateService namespaceCreateService;
+  private final NamespaceDeleteService namespaceDeleteService;
+
+  @EventHook
+  private void applicationLaunch(ApplicationLaunchEvent event) {
+    serviceRepository.register(NamespaceListRequest.class,
+      namespaceListService);
+    serviceRepository.register(NamespaceCreateRequest.class,
+      namespaceCreateService);
+    serviceRepository.register(NamespaceDeleteRequest.class,
+      namespaceDeleteService);
+  }
+}
